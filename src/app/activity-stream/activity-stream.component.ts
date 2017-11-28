d190816c53df823e124519531c514c4341393676
Jeff Mixter <jeffmixter@gmail.com>
2017-11-28 17:32:01 -0500

    added paging for large results

--- src/app/activity-stream/activity-stream.component.ts
+++ src/app/activity-stream/activity-stream.component.ts
@@ -24,48 +24,55 @@ export class ActivityStreamComponent implements OnInit {
           this.jsonData['@context'] = [];
           this.jsonData['@context'].push('http://iiif.io/api/presentation/3/context.json');
           this.jsonData['@context'].push('https://www.w3.org/ns/activitystreams');
-          this.jsonData['id'] = 'http://localhost:3071/activity_stream';
+          this.jsonData['id'] = 'http://52.204.112.237/iiif_activity_streams/activity_stream/';
           this.jsonData['type'] = 'Collection';
           this.jsonData['label'] = 'CONTENTdm IIIF Collections';
           this.jsonData['first'] = {};
-          this.jsonData['first']['id'] = 'http://localhost:3071/activity_stream/1';
+          this.jsonData['first']['id'] = 'http://52.204.112.237/iiif_activity_streams/activity_stream/1';
           this.jsonData['first']['type'] = 'CollectionPage';
-          var url = 'http://localhost:9200/activity_streams/_stats';
+          var url = 'http://52.204.112.237:3061/?action=count';
           this.httpService.getJson(url).then(data => {
-              this.jsonData['total'] = data['_all']['primaries']['docs']['count'];
-              var last = this.jsonData['total'] / 500;
+              var total = data.count;
+              console.log(total);
+              var last = Number(total) / 5000;
               if (last % 1 != 0){
                   var lastPage = Math.floor(last + 1);
                   this.jsonData['last'] = {};
-                  this.jsonData['last']['id'] = 'http://localhost:3071/activity_stream/'+lastPage;
+                  this.jsonData['last']['id'] = 'http://52.204.112.237/iiif_activity_streams/activity_stream/'+lastPage;
                   this.jsonData['last']['type'] = 'CollectionPage';
               }
               else{
                   var lastPage = Math.floor(last);
                   this.jsonData['last'] = {};
-                  this.jsonData['last']['id'] = 'http://localhost:3071/activity_stream/'+last;
+                  this.jsonData['last']['id'] = 'http://52.204.112.237/iiif_activity_streams/activity_stream/'+last;
                   this.jsonData['last']['type'] = 'CollectionPage'; 
               }
               console.log(last)
-          });
+           });
       }
       else{
           this.jsonData = {};
-          if (this.page == 1){
-              var startingItem = 0;
-          }
-          else {
-              var startingItem = this.page * 500 + 1; 
-          }
-          var url = 'http://52.204.112.237:3061/?action=search&size=500&from=' + startingItem;
+          var url = 'http://52.204.112.237:3061/?action=paging&page=' + this.page;
           console.log(url);
+          var nextPage = (Number(this.page) + Number(1));
+          var previousPage = (Number(this.page) - Number(1));
+          console.log(previousPage);
+          console.log(nextPage);
           this.httpService.getJson(url).then(data => {
              this.jsonData = data;
              //this.jsonData['type'] = 'CollectionPage';
              this.jsonData['partOf'] = 'http://52.204.112.237/iiif_activity_streams/activity_stream/';
-             this.jsonData['next'] = {};
-             this.jsonData['next']['type'] = 'CollectionPage';
-             this.jsonData['next']['type'] = this.page + 1;
+             if (data.items.length === 500) {
+                 this.jsonData['next'] = {};
+                 this.jsonData['next']['type'] = 'CollectionPage';
+                 this.jsonData['next']['id'] = 'http://52.204.112.237/iiif_activity_streams/activity_stream/' + nextPage;
+             }
+             if (this.page > 1) {
+                 this.jsonData['previous'] = {};
+                 this.jsonData['previous']['type'] = 'CollectionPage';
+                 this.jsonData['previous']['id'] = 'http://52.204.112.237/iiif_activity_streams/activity_stream/' + previousPage;
+             }
+             
           });
       }
   }
